361a4ce23734546752dccac40f4dc13b1cbd31cd
Stepan <stevedekart2020@gmail.com>
2025-10-08 15:37:47 +0200

    test

--- src/pages/IndexPage/constants/whoweare.ts
+++ src/pages/IndexPage/constants/whoweare.ts
@@ -2,6 +2,7 @@ import pc from "@/assets/pc.svg";
 
 export const WhoWeAre = {
   image: pc,
+
   content: `We’re a small team of developers who created this project as both a portfolio piece and a solution to a real problem we faced.
 
 We needed a simple, reliable subscription tracker with notifications — without the complexity or privacy concerns of other apps.
